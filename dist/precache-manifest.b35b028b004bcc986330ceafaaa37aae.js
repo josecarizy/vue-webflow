ef3006cc7a1db834a9f3b9a52ce15ab4ad547add
josecarizy <jose@carizy.com>
2021-03-31 11:46:28 +0200

    whit dist folder

---
+++ dist/precache-manifest.b35b028b004bcc986330ceafaaa37aae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a9b3c120c5f99b04b9",
+    "url": "/css/app.f0ab0b4e.css"
+  },
+  {
+    "revision": "6412030fc7b1c2102ce4",
+    "url": "/css/chunk-vendors.b123565c.css"
+  },
+  {
+    "revision": "928de4189c36a49ea6ac00eb36bbda98",
+    "url": "/image.png"
+  },
+  {
+    "revision": "d19dac9cf4c66c7efec406d886cb4dd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a9b3c120c5f99b04b9",
+    "url": "/js/app.ac23c2ab.js"
+  },
+  {
+    "revision": "6412030fc7b1c2102ce4",
+    "url": "/js/chunk-vendors.aefd753d.js"
+  },
+  {
+    "revision": "738fc970c22849d004c9cae4e06bc36e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
